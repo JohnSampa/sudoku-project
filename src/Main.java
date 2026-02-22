5baafdb2ccd2a5d59da32762f56b686e753c5fb8
Jonatas Sampaio <aurijona192@gmail.com>
2026-02-21 22:56:50 -0300

    Inicializando projeto

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import br.com.dio.util.BoardTemplate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(BoardTemplate.BOARD_TEMPLATE );
+    }
+}
---
+++ src/br/com/dio/model/Board.java
@@ -0,0 +1,71 @@
+package br.com.dio.model;
+
+import java.util.Collection;
+import java.util.List;
+
+import static br.com.dio.model.GameStatusEnum.COMPLETE;
+import static br.com.dio.model.GameStatusEnum.INCOMPLETE;
+import static br.com.dio.model.GameStatusEnum.NON_STARTED;
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
+public class Board {
+
+    private final List<List<Space>> spaces;
+
+    public Board(List<List<Space>> spaces) {
+        this.spaces = spaces;
+    }
+
+    public boolean changeValue(int col, int row, int value) {
+        Space space = spaces.get(col).get(row);
+
+        if(space.isFixed()) return false;
+
+        space.setActual(value);
+        return true;
+    }
+
+    public boolean clearValue(int col, int row) {
+        Space space = spaces.get(col).get(row);
+
+        if(space.isFixed()) return false;
+
+        space.clearSpace();
+        return true;
+    }
+
+    public void reset(){
+        spaces.forEach(s -> s.forEach(Space::clearSpace));
+    }
+
+    public boolean gameIsFinished() {
+        return hasErros() && getStatus() == COMPLETE;
+    }
+
+    public boolean hasErros(){
+        if(getStatus() == NON_STARTED)return false;
+
+        return spaces.stream().flatMap(Collection::stream)
+                .anyMatch(x -> nonNull(x.getActual()) && x.getActual().equals(x.getActual()));
+    }
+
+    public List<List<Space>> getSpaces() {
+        return spaces;
+    }
+
+    public GameStatusEnum getStatus() {
+        if(
+                spaces.stream()
+                        .flatMap(Collection::stream)
+                        .noneMatch(x -> !x.isFixed()&& nonNull(x.getActual()))
+        ){
+            return NON_STARTED;
+        }
+
+        return spaces.stream()
+                .flatMap(Collection::stream)
+                .anyMatch(x -> isNull(x.getActual()))
+                ?INCOMPLETE:COMPLETE;
+    }
+}
